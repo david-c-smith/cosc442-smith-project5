fdcaf8e551cbf692cdf8b4adf5822f5117bcad9a
david-c-smith <dcsmith0515@gmail.com>
2017-04-20 15:14:37 -0400

    Task 2 – VHDL Bit Stream Detector - Input and output txt files

--- WMethod/WMethod.java
+++ WMethod/WMethod.java
@@ -381,12 +381,7 @@ public class WMethod{
      Vector <String> tests=generateTests(transitionCover, w); // Generate tests.
      Utilities.printAllTestCases(tests); // Print tests.
      
-     // TODO: 	Write the necessary code to iterate through all test cases and run them against
-     // 		the FSM using the Utilities.runFSM() method. 
-     //
-     // Example use of the Utilities.runFSM() method
-     // Utilities.runFSM(FSM, 1, "a a b a b", " ");
-     
+     //For each loop iterates through and fixes "" by changing to " "
      for(String test: tests) {
     	 test = test.replace(""," ");
     	 Utilities.runFSM(FSM, startState, test, " ");
